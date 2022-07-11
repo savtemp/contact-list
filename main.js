77b47905fd8a4699ed07f30bc9ad48ac38f0dcc1
savtemp <savtemple@gmail.com>
2022-07-10 22:24:26 -0600

    toggle buttons

---
+++ main.js
@@ -0,0 +1,75 @@
+let contacts = []
+
+
+function addContact(event) {
+  event.preventDefault()
+  let form = event.target
+
+  let contact = {
+    id: generateId(),
+    name: form.name.value,
+    phone: form.phone.value,
+    emergencyContact: form.emergencyContact.checked
+  }
+  contacts.push(contact)
+  saveContacts()
+  form.reset()
+}
+
+function saveContacts() {
+  window.localStorage.setItem("contacts", JSON.stringify(contacts))
+  drawContacts()
+}
+
+function loadContacts() {
+  let storedContacts = JSON.parse(window.localStorage.getItem("contacts"))
+  if (storedContacts) {
+    contacts = storedContacts
+  }
+}
+
+function drawContacts() {
+  let contactListElement = document.getElementById("contact-list")
+  let contactsTemplate = ""
+  contact.forEach(contact => {
+    contactsTemplate += `
+    <div class="contact-card mt-1 mb-1 ${contact.emergencyContact ? 'emergency-contact' : ''}">
+    <h3 class="mt-1 mb-1">${contact.name}</h3>
+    <div class="d-flex space-between">
+      <p>
+        <i class="fa fa-fw fa-phone"></i>
+        <span>${contact.phone}</span>
+      </p>
+      <i class="action fa fa-trash text-danger" onclick="removeContact('${contact.id}')"></i>
+    </div>
+  </div>
+    `
+  })
+  contactListElement.innerHTML = contactsTemplate
+}
+
+function removeContact(contactId) {
+  let index = contacts.findIndex(contact => contact.id == contactId)
+  if(index == -1) {
+    throw new Error("Invalid Contact Id")
+  }
+  contacts.splice(index, 1)
+  saveContacts()
+}
+
+function toggleAddContactForm() {
+  document.getElementById('new-contact-form').classList.toggle("hidden")
+}
+
+/**
+ * Used to generate a random string id for mocked
+ * database generated Id
+ * @returns {string}
+ */
+function generateId() {
+  return Math.floor(Math.random() * 10000000) + "-" + Math.floor(Math.random() * 10000000)
+}
+
+
+loadContacts()
+drawContacts()
